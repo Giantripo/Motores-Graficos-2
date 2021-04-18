878eef0d8aace6d199cf7e05e434e846b5737259
Gian Franco <giancapo2011@gmail.com>
2021-04-18 02:55:54 -0300

    new record hecho, falta el canvas y la persistencia

--- template_runner/Assets/Scripts/Bullet.cs
+++ template_runner/Assets/Scripts/Bullet.cs
@@ -23,4 +23,5 @@ public class Bullet : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+    
 }
--- template_runner/Assets/Scripts/Controller_Hud.cs
+++ template_runner/Assets/Scripts/Controller_Hud.cs
@@ -6,16 +6,21 @@ public class Controller_Hud : MonoBehaviour
     public static bool gameOver = false;
     public Text distanceText;
     public Text gameOverText;
-    public float distance;
-    public float maxDistanceHud;
+    public float distance = 0;
+    public float maxDistance = 0;
+    public float newPM = 0;
+
+
 
     void Start()
     {
         
         gameOver = false;
-        //distance = 0;
+        distance = 0;
         distanceText.text = distance.ToString();
         gameOverText.gameObject.SetActive(false);
+        
+        
     }
 
     void Update()
@@ -26,25 +31,32 @@ public class Controller_Hud : MonoBehaviour
             Time.timeScale = 0;
             gameOverText.text = "Game Over \n Total Distance: " + distance.ToString();
             gameOverText.gameObject.SetActive(true);
+
+            if (Persistencia.instancia.data.record < maxDistance)
+            {
+                Persistencia.instancia.data.record = maxDistance;
+                Debug.Log("nuevo puntaje maximo" + Persistencia.instancia.data.record);
+            }
+            
+           
+
         }
         //si gameOver sigue siendo true aumenta la distancia y la imprime en pantalla con la UI
         else
         {
             distance += Time.deltaTime;
+          
             //transformo float en int
             int distanceToInt = (int)distance;
             distanceText.text = distanceToInt.ToString();
 
         }
 
-        maxDistanceHud = 5;
-
-        Persistencia.instancia.data.maxDistance += Time.deltaTime;
-
-        if(maxDistanceHud < Persistencia.instancia.data.maxDistance)
+        
+        if(distance > maxDistance && distance> Persistencia.instancia.data.record)
         {
-            
-            Debug.Log("superaste el record");
+            maxDistance = distance;
         }
+        
     }
 }
